78a6a0761983751a6bf552690e0ed40d9e3426ad
Arturo Arteaga Villarroel <Arturo@iMac-de-Arturo.local>
2016-09-01 20:41:28 -0500

    Hola

--- empleados/js/script.js
+++ empleados/js/script.js
@@ -4,6 +4,7 @@ $().ready(function(){
 
 	// INICIALMENTE LOS FILL-IN DEBEN VENIR VACÍOS
 	$("#mLogin").click(function(){
+		$("#mLogin").html('Inicie sesión');
 		$("#cveEmpresa" ).val("");
 		$("#cveEmpleado").val("");
 		$("#contrasena" ).val("");
@@ -17,17 +18,19 @@ $().ready(function(){
 						 cveEmpleado:$("#cveEmpleado").val(),
 						 contrasena:$("#contrasena").val()};
 		$.get("login.json",datosJSON,function(respuesta){
-			console.log(respuesta);
 			if (respuesta.resultado != "0") {
 				alert(respuesta.texto);
 				return false;
 			}
 			sessionStorage.setItem("sessionId",respuesta.sessionId);
+			if (respuesta.empresa != "") {$("#empresa").html(respuesta.empresa)};
+			$("#mLogin").html('');
 			$("#mensajeEmpresa").html(respuesta.mensajeEmpresa);
 			$("#mensajeEmpleado").html(respuesta.mensajeEmpleado);
 			$("#mNombre").html(respuesta.nombre + '<span class="caret"></span>');
 			$("#btnLoginCierra").click();
 			$("#actual-0").click();
+			$("#actual").focus();
 		}).error(function(e){
 			alert("Servicio no disponible. Inténtelo más tarde\n(" + e.status + ": " + e.statusText + ")");
 		});
@@ -54,44 +57,105 @@ $().ready(function(){
 		leeRecibos(this.text);
 	});
 
-	function leeRecibos(ejercicio) {
-		$(".contenido").html('<img src="../images/cargando.gif">');
-		$.get("recibos.html","",function(recibosHtml){
-			var datosJSON = {sessionId:sessionStorage.getItem("sessionId"),
-							 ejercicio:ejercicio};
-			$.get("recibos.json",datosJSON,function(respuesta){
-				if (respuesta[0].resultado != "0") {
-					alert(respuesta.texto);
-					return false;
-				}
-				$(".contenido").html(recibosHtml);
-				for (var i=1;i<respuesta.length;i++) {
-					var renglon='<tr>'
-					           +'<td>' + respuesta[i].nomina    + '</td>'
-					           +'<td>' + respuesta[i].periodo   + '</td>'
-					           +'<td>' + respuesta[i].depto     + '</td>'
-					           +'<td>' + respuesta[i].puesto    + '</td>'
-					           +'<td>' + respuesta[i].descargas + '</td>'
-					           +'<td><a class="enviar" recibo="' + respuesta[i].recibo + '" href="#"><img src="../images/eMail.jpg" width="30" height="30"></a></td>' 
-							   +'</tr>';
-					$(".table").append(renglon);
-				};
-			}).error(function(e){
-				alert("Servicio no disponible. Inténtelo más tarde\n(" + e.status + ": " + e.statusText + ")");			
-			});
+	$("#mMail").click(function(){
+		var datosJSON={sessionId:sessionStorage.getItem("sessionId")};
+		$.get("eMail.json",datosJSON,function(respuesta){
+			if (respuesta.resultado != "0") {
+				alert(respuesta.texto);
+				return false;
+			}
+			$("#eMail").val(respuesta.eMail);
 		}).error(function(e){
-			alert("Snippet de recibos no disponible. Notifíquelo a Soporte\n(" + e.status + ": " + e.statusText + ")");						
+			alert("Servicio no disponible. Inténtelo más tarde\n(" + e.status + ": " + e.statusText + ")");			
 		});
-	}
+	});
+
+	$("#btnMail").click(function(){
+		var datosJSON={sessionId:sessionStorage.getItem("sessionId"),
+					   eMail:$("#eMail").val()};
+		$.get("eMailActualiza.json",datosJSON,function(respuesta){
+			if (respuesta.resultado != "0") {
+				alert(respuesta.texto);
+				return false;
+			};
+			$("#btnMailCierra").click();
+			alert("¡eMail actualizado exitosamente!");
+		}).error(function(e){
+			alert("Servicio no disponible. Inténtelo más tarde\n(" + e.status + ": " + e.statusText + ")");			
+		});
+	});
+
+	$("#btnContrasena").click(function(){
+		var datosJSON={sessionId:sessionStorage.getItem("sessionId"),
+					   anterior:$("#contrasenaAnterior").val(),
+					   nueva:$("#nuevaContrasena").val(),
+					   nueva:$("#repitaContrasena").val()};
+		$.get("contrasenaActualiza.json",datosJSON,function(respuesta){
+			if (respuesta.resultado != "0") {
+				alert(respuesta.texto);
+				return false;
+			};
+			$("#btnContrasenaCierra").click();
+			alert("¡Contraseña actualizada exitosamente!");
+		}).error(function(e){
+			alert("Servicio no disponible. Inténtelo más tarde\n(" + e.status + ": " + e.statusText + ")");			
+		});
+	});
 
 	var ahorita = new Date();
-	$("#actual").html(ahorita.getFullYear() + ' <span class="caret"></span>');
-	$("#actual-0").html(ahorita.getFullYear() - 0);
-	$("#actual-1").html(ahorita.getFullYear() - 1);
-	$("#actual-2").html(ahorita.getFullYear() - 2);
-	$("#actual-3").html(ahorita.getFullYear() - 3);
-	$("#actual-4").html(ahorita.getFullYear() - 4);
+	var ejercicio = ahorita.getFullYear();
+	$("#actual").html(ejercicio + ' <span class="caret"></span>');
+	$("#actual-0").html(ejercicio - 0);
+	$("#actual-1").html(ejercicio - 1);
+	$("#actual-2").html(ejercicio - 2);
+	$("#actual-3").html(ejercicio - 3);
+	$("#actual-4").html(ejercicio - 4);
 
 	$("#mLogin").click();
 
 });
+
+function leeRecibos(ejercicio) {
+	$(".contenido").html('<img src="../images/cargando.gif">');
+	$.get("recibos.html","",function(recibosHtml){
+		var datosJSON = {sessionId:sessionStorage.getItem("sessionId"),
+						 ejercicio:ejercicio};
+		$.get("recibos.json",datosJSON,function(respuesta){
+			if (respuesta[0].resultado != "0") {
+				alert(respuesta[0].texto);
+				return false;
+			}
+			$(".contenido").html(recibosHtml);
+			for (var i=1;i<respuesta.length;i++) {
+				var renglon='<tr>'
+				           +'<td>' + respuesta[i].nomina    + '</td>'
+				           +'<td>' + respuesta[i].periodo   + '</td>'
+				           +'<td>' + respuesta[i].depto     + '</td>'
+				           +'<td>' + respuesta[i].puesto    + '</td>'
+				           +'<td>' + respuesta[i].descargas + '</td>'
+				           +'<td><img src="../images/eMail.jpg" width="30" height="30" onclick="enviar(' + "'" + respuesta[i].recibo + "'" + ')"></td>' 
+						   +'</tr>';
+				$(".table").append(renglon);
+			};
+		}).error(function(e){
+			alert("Servicio no disponible. Inténtelo más tarde\n(" + e.status + ": " + e.statusText + ")");			
+		});
+	}).error(function(e){
+		alert("Snippet de recibos no disponible. Notifíquelo a Soporte\n(" + e.status + ": " + e.statusText + ")");						
+	});
+};
+
+function enviar(cual) {
+	var datosJSON = {sessionId:sessionStorage.getItem("sessionId"),
+					 recibo:cual};
+	$.get("enviar.json",datosJSON,function(respuesta){
+		if (respuesta.resultado != "0") {
+			alert(respuesta.texto);
+			return false;
+		};
+		alert("Recibo enviado a su correo.");
+	}).error(function(e){
+		alert("Servicio no disponible. Inténtelo más tarde\n(" + e.status + ": " + e.statusText + ")");			
+	});
+};
+
